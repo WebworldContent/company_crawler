d97c22b84389132ee94a1e4fcfe849f81f7cbf97
Aamir793 <amiraftab1997@gmail.com>
2024-06-18 22:47:06 +0530

    add all db support

--- src/models/companies.ts
+++ src/models/companies.ts
@@ -35,7 +35,45 @@ export const getAllCompanies = async (): Promise<QueryResult> => {
     const [rows] = response;
     return rows;
   } catch (err) {
-    console.log("Error inserting data", err);
+    console.log("Error fetching data", err);
+    throw err;
+  }
+};
+
+export const getACompany = async (id: string): Promise<QueryResult> => {
+  try {
+    const response = await conn((connection: any) => {
+      if (connection) {
+        return connection?.execute(
+          `select * from companies where id = ${id}`
+        );
+      }
+    });
+
+    if (!response) {
+      throw new Error("No response from database");
+    }
+
+    const [rows] = response;
+    return rows;
+  } catch (err) {
+    console.log("Error fetching data", err);
+    throw err;
+  }
+};
+
+export const DeleteACompany = async (id: string): Promise<void> => {
+  try {
+    await conn((connection: any) => {
+      if (connection) {
+        return connection?.execute(
+          `delete from companies where id = ${id}`
+        );
+      }
+    });
+
+  } catch (err) {
+    console.log("Error fetching data", err);
     throw err;
   }
 };
